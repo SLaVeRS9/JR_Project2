c2531f06f2c072de63aa0cd3c2c77a63376b2b4d
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-05-10 12:40:27 +0300

    Added island simulation properties

---
+++ src/SimulatorProperties/SimulationProperties.java
@@ -0,0 +1,7 @@
+package SimulatorProperties;
+
+public class SimulationProperties {
+    private static final Integer ISLAND_HEIGHT = 20;
+    private static final Integer ISLAND_WIDTH = 10;
+    private static final Integer TIME_ON_STEP_IN_MILLISECONDS = 1000;
+}
