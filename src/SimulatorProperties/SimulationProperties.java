5d83111ed70a1c1e486724ced8fe928e143f8f0f
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-05-23 01:43:56 +0300

    Refactoring SimulationProperties

--- src/SimulatorProperties/SimulationProperties.java
+++ src/SimulatorProperties/SimulationProperties.java
@@ -1,7 +1,57 @@
 package SimulatorProperties;
 
+import Biosphere.Animal;
+import Biosphere.Animals.Herbivores.Sheep;
+import Biosphere.Animals.Predators.Fox;
+import Biosphere.Animals.Predators.Wolf;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimulationProperties {
-    private static final Integer ISLAND_HEIGHT = 20;
-    private static final Integer ISLAND_WIDTH = 10;
-    private static final Integer TIME_ON_STEP_IN_MILLISECONDS = 1000;
+
+    // Program params
+    public static final int TIME_ON_STEP_IN_MILLISECONDS = 3000;
+    public static final int TIME_ON_STEP_IN_SECONDS = TIME_ON_STEP_IN_MILLISECONDS / 1000;
+    public static final int THREADS_COUNT = 1;
+
+    // Island params
+    public static final int ISLAND_HEIGHT = 3;
+    public static final int ISLAND_WIDTH = 3;
+
+    // Cell param
+    public static final int MAX_ANIMALS_IN_CELL_ON_START = 120;
+    public static final int MAX_PLANTS_IN_CELL_ON_START = 100;
+
+    // Biosphere params
+    private static final int HERBIVORES_KINDS_STARTING_AMOUNT = 10;
+    public static final int HERBIVORES_STARTING_AMOUNT = HERBIVORES_KINDS_STARTING_AMOUNT * 2;
+
+    private static final int PREDATORS_KINDS_STARTING_AMOUNT = 5;
+    public static final int PREDATORS_STARTING_AMOUNT = PREDATORS_KINDS_STARTING_AMOUNT * 2;
+
+    private static final int PLANTS_KINDS_STARTING_AMOUNT = 2;
+    public static final int PLANTS_STARTING_AMOUNT = PLANTS_KINDS_STARTING_AMOUNT * 20;
+
+
+    // Animals params
+    public static final double HEALTH_PERCENT_DECREASE_FROM_HUNGER = 0.1;
+
+    public static List<Animal> startAnimalsPopulation = new ArrayList<>(40) {{
+        add(new Wolf());
+        add(new Wolf());
+        add(new Wolf());
+        add(new Wolf());
+        add(new Fox());
+        add(new Fox());
+        add(new Fox());
+        add(new Fox());
+        add(new Sheep());
+        add(new Sheep());
+        add(new Sheep());
+        add(new Sheep());
+    }};
+
+    // Plants params
+    public static final int PERCENT_OF_CHANCE_NEW_PLANTS_GROW_IN_CELL = 15;
 }
