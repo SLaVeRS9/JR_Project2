02cc02556df233d4a6a19322c0e981e4de493697
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-05-23 08:47:08 +0300

    Add Main class with start() method and TODO list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import Simulation.Simulation;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Simulation.start();
+
+        //TODO Stopping threads logic
+        //TODO Schedulers hierarchy
+
+        //TODO Add new animals classes
+        //TODO Add new animals properties
+
+        //TODO [OPTIONAL] All parameters into properties
+        //TODO Readme
     }
 }
