8aedcf1b6d74e5c409f27560c385556370272b3c
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-05-27 18:36:12 +0300

    Clear method Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import biosphere.Animal;
+import biosphere.BiosphereTypes;
+import island.Direction;
+import island.Island;
 import simulation.Simulation;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Simulation.start();
-
-        //TODO Stopping threads logic
-        //TODO Schedulers hierarchy
-
         //TODO Add new animals classes
         //TODO Add new animals properties
 
         //TODO [OPTIONAL] All parameters into properties
         //TODO Readme
+
     }
 }
