074df3fa69ef23a90b96e8e3d993d98242f5548e
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-05-28 20:40:23 +0300

    Add classification in enum BiosphereTypes (for next features)

--- src/biosphere/BiosphereTypes.java
+++ src/biosphere/BiosphereTypes.java
@@ -1,36 +1,31 @@
 package biosphere;
 
 public enum BiosphereTypes {
-    WOLF ("\uD83D\uDC3A"),
-    FOX ("\uD83E\uDD8A"),
-    SNAKE("\uD83D\uDC0D"),
-    BEAR("\uD83D\uDC3B"),
-    EAGLE("\uD83E\uDD85"),
-    HORSE("\uD83D\uDC0E"),
-    DEER("\uD83E\uDD8C"),
-    RABBIT("\uD83D\uDC07"),
-    MOUSE("\uD83D\uDC01"),
-    GOAT("\uD83D\uDC10"),
-    SHEEP("\uD83D\uDC11"),
-    BOAR("\uD83D\uDC17"),
-    BUFFALO("\uD83D\uDC02"),
-    DUCK("\uD83E\uDD86"),
-    CATERPILLAR("\uD83D\uDC1B"),
-    BUSH("\uD83C\uDF3E"),
-    GRASS("\uD83C\uDF3F");
+    WOLF ("\uD83D\uDC3A", "Predator"),
+    FOX ("\uD83E\uDD8A", "Predator"),
+    SNAKE("\uD83D\uDC0D", "Predator"),
+    BEAR("\uD83D\uDC3B", "Predator"),
+    EAGLE("\uD83E\uDD85", "Predator"),
+    HORSE("\uD83D\uDC0E", "Herbivore"),
+    DEER("\uD83E\uDD8C", "Herbivore"),
+    RABBIT("\uD83D\uDC07", "Herbivore"),
+    MOUSE("\uD83D\uDC01", "Herbivore"),
+    GOAT("\uD83D\uDC10", "Herbivore"),
+    SHEEP("\uD83D\uDC11", "Herbivore"),
+    BOAR("\uD83D\uDC17", "Herbivore"),
+    BUFFALO("\uD83D\uDC02", "Herbivore"),
+    DUCK("\uD83E\uDD86", "Herbivore"),
+    CATERPILLAR("\uD83D\uDC1B", "Herbivore"),
+    BUSH("\uD83C\uDF3E", "Plant"),
+    GRASS("\uD83C\uDF3F", "Plant");
 
-    private String unicode;
+    private final String UNICODE;
 
-    /*@Override
-    public Integer getDieChanceFrom(BiosphereTypes predator) {
-        return EatingProperties.getEatingChances(this, predator);
-    }*/
-
-    BiosphereTypes(String unicode) {
-        this.unicode = unicode;
+    BiosphereTypes(String unicode, String classification) {
+        UNICODE = unicode;
     }
 
     public String getUnicode(){
-        return unicode;
+        return UNICODE;
     }
 }
